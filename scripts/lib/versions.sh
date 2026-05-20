9f18fc86796582756cbf8eae208cd154fb0171ea
Adam Leskis <leskis@gmail.com>
2026-05-20 14:50:46 +0100

    Update manifest versions

--- scripts/lib/versions.sh
+++ scripts/lib/versions.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-readonly DEFAULT_FIRST_PARTY_IMAGE_TAG="v6"
+readonly DEFAULT_FIRST_PARTY_IMAGE_TAG="v7"
 readonly ROOTFS_IMAGE_REPO="ghcr.io/lpmi-13/argo-remotelab-k3s-rootfs"
-readonly DEFAULT_IXIMIUZ_ROOTFS_IMAGE="ghcr.io/lpmi-13/argo-remotelab-k3s-rootfs:v6"
+readonly DEFAULT_IXIMIUZ_ROOTFS_IMAGE="ghcr.io/lpmi-13/argo-remotelab-k3s-rootfs:v7"
 readonly DEFAULT_IXIMIUZ_ROOTFS_RELEASE="e015d4ac.1"
 
 readonly DJANGO_IMAGE_REPO="ghcr.io/lpmi-13/argo-remotelab-django"
